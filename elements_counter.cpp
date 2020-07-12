877244f617c26ef8311a3a2f345bf1c9fcae517e
Ivan Terentev <gmvano@gmail.com>
2020-07-12 17:31:54 +0300

    Adds executable with function prototypes

---
+++ elements_counter.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+std::vector<std::string> downloadDocuments(const std::vector<std::string> &urls) {
+    return {};
+}
+
+std::vector<int> countElements(const std::vector<std::string> &documents) {
+    return {};
+}
+
+void printCounts(const std::vector<int> &counts, const std::vector<std::string> &urls) {
+}
+
+int main(int argc, char **argv) {
+    if (argc <= 1) {
+        std::cerr << "Usage: elements_counter <list of URLs leading to xml documents separated with space>" << std::endl;
+        return 1;
+    }
+    std::vector<std::string> urls(argv + 1, argv + argc);
+
+    const auto documents = downloadDocuments(urls);
+
+    const auto element_counts = countElements(documents);
+
+    printCounts(element_counts, urls);
+
+    return 0;
+}
